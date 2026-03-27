aae6b0a24d313b7250c934a7b13ff9662e594a13
Sumit <sumitrathod1992004@gmail.com>
2026-03-27 08:37:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let height,weight,BMI;
+document.getElementById("submit").onclick = function(){
+  height = document.getElementById("input1").value ;
+  height = Number(height);
+  height =  (height * 0.3048);
+  height = height*height;
+  weight = document.getElementById("input2").value ;
+  weight = Number(weight);
+  BMI = weight / height;
+  console.log(BMI);
+}
